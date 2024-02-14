edf8465a3fec6d94d3edb2d6e6d8d179f5a84c71
aziz bahri <azizbahri90@gmail.com>
2024-02-14 21:11:58 +0000

    argparser
    Signed-off-by: aziz bahri <azizbahri90@gmail.com>

--- examples/SX126x/transmitter.py
+++ examples/SX126x/transmitter.py
@@ -1,8 +1,18 @@
 from LoRaRF import SX126x
 import time
+import argparse
 
-def main(LoRa = None):
+def main(LoRa = None, frequency = 868000000, sf = 7, bw = 125000, power = 22, cr = 5):
 
+    # Print configuration to console
+    print("\n-- LoRa Transmitter Configuration --")
+    print(f"Frequency = {frequency} Hz")
+    print(f"Spreading factor = {sf}")
+    print(f"Bandwidth = {bw} Hz")
+    print(f"Transmit power = +{power} dBm")
+    print(f"Coding rate = {cr}")
+    
+    
     # Begin LoRa radio and set NSS, reset, busy, IRQ, txen, and rxen pin with connected Raspberry Pi gpio pins
     # IRQ pin not used in this example (set to -1). Set txen and rxen pin to -1 if RF module doesn't have one
     busId = 0; csId = 0 
@@ -13,21 +23,16 @@ def main(LoRa = None):
         raise Exception("Something wrong, can't begin LoRa radio")
 
     LoRa.setDio2RfSwitch()
-    # Set frequency to 868 Mhz
-    print("Set frequency to 868 Mhz")
-    LoRa.setFrequency(868000000)
+    # Set frequency 
+    LoRa.setFrequency(frequency)
 
     # Set TX power, default power for SX1262 and SX1268 are +22 dBm and for SX1261 is +14 dBm
     # This function will set PA config with optimal setting for requested TX power
-    print("Set TX power to +22 dBm")
-    LoRa.setTxPower(22, LoRa.TX_POWER_SX1262)                       # TX power +17 dBm using PA boost pin
+    LoRa.setTxPower(power, LoRa.TX_POWER_SX1262)                       # TX power +17 dBm using PA boost pin
 
     # Configure modulation parameter including spreading factor (SF), bandwidth (BW), and coding rate (CR)
     # Receiver must have same SF and BW setting with transmitter to be able to receive LoRa packet
     print("Set modulation parameters:\n\tSpreading factor = 7\n\tBandwidth = 125 kHz\n\tCoding rate = 4/5")
-    sf = 7                                                          # LoRa spreading factor: 7
-    bw = 125000                                                     # Bandwidth: 125 kHz
-    cr = 5                                                          # Coding rate: 4/5
     LoRa.setLoRaModulation(sf, bw, cr)
 
     # Configure packet parameter including header type, preamble length, payload length, and CRC type
@@ -77,8 +82,33 @@ def main(LoRa = None):
 
 if __name__ == "__main__":
     try:
+        parser = argparse.ArgumentParser(description='LoRa Transmitter') 
+        parser.add_argument('--frequency', type=int, help='Frequency in Hz')
+        parser.add_argument('--spreading_factor', type=int, help='Spreading factor') 
+        parser.add_argument('--bandwidth', type=int, help='Bandwidth in Hz') 
+        parser.add_argument('--power', type=int, help='Transmit power in dBm') 
+        parser.add_argument('--cr', type=int, help='Codeing rate') 
+        
+        args = parser.parse_args() 
+        if args.cr:
+            cr = args.cr
+        else: cr = 5
+        if args.spreading_factor: 
+            sf = args.spreading_factor 
+        else: sf = 7 
+        if args.bandwidth: 
+            bw = args.bandwidth 
+        else: bw = 125000 
+        if args.power: 
+            power = args.power 
+        else: power = 22
+        args = parser.parse_args()
+        if args.frequency:
+            frequency = args.frequency
+        else:
+            frequency = 868000000
         LoRa = SX126x()
-        main(LoRa)
+        main(LoRa, frequency, sf, bw, power, cr)
     except KeyboardInterrupt:
         print("Program terminated by user")
     finally:
