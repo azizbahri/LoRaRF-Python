784e3fcf8249075bbcb6e7b543557e18154d9332
aziz bahri <azizbahri90@gmail.com>
2024-02-14 20:53:00 +0000

    wrap things up in a function
    Signed-off-by: aziz bahri <azizbahri90@gmail.com>

--- examples/SX126x/transmitter.py
+++ examples/SX126x/transmitter.py
@@ -1,79 +1,85 @@
 from LoRaRF import SX126x
 import time
 
-# Begin LoRa radio and set NSS, reset, busy, IRQ, txen, and rxen pin with connected Raspberry Pi gpio pins
-# IRQ pin not used in this example (set to -1). Set txen and rxen pin to -1 if RF module doesn't have one
-busId = 0; csId = 0 
-resetPin = 18; busyPin = 20; irqPin = 16; txenPin = 6; rxenPin = -1 
-LoRa = SX126x()
-print("Begin LoRa radio")
-if not LoRa.begin(busId, csId, resetPin, busyPin, irqPin, txenPin, rxenPin) :
-    raise Exception("Something wrong, can't begin LoRa radio")
+def main(LoRa = None):
 
-LoRa.setDio2RfSwitch()
-# Set frequency to 868 Mhz
-print("Set frequency to 868 Mhz")
-LoRa.setFrequency(868000000)
+    # Begin LoRa radio and set NSS, reset, busy, IRQ, txen, and rxen pin with connected Raspberry Pi gpio pins
+    # IRQ pin not used in this example (set to -1). Set txen and rxen pin to -1 if RF module doesn't have one
+    busId = 0; csId = 0 
+    resetPin = 18; busyPin = 20; irqPin = 16; txenPin = 6; rxenPin = -1 
+    
+    print("Begin LoRa radio")
+    if not LoRa.begin(busId, csId, resetPin, busyPin, irqPin, txenPin, rxenPin) :
+        raise Exception("Something wrong, can't begin LoRa radio")
 
-# Set TX power, default power for SX1262 and SX1268 are +22 dBm and for SX1261 is +14 dBm
-# This function will set PA config with optimal setting for requested TX power
-print("Set TX power to +22 dBm")
-LoRa.setTxPower(22, LoRa.TX_POWER_SX1262)                       # TX power +17 dBm using PA boost pin
+    LoRa.setDio2RfSwitch()
+    # Set frequency to 868 Mhz
+    print("Set frequency to 868 Mhz")
+    LoRa.setFrequency(868000000)
 
-# Configure modulation parameter including spreading factor (SF), bandwidth (BW), and coding rate (CR)
-# Receiver must have same SF and BW setting with transmitter to be able to receive LoRa packet
-print("Set modulation parameters:\n\tSpreading factor = 7\n\tBandwidth = 125 kHz\n\tCoding rate = 4/5")
-sf = 7                                                          # LoRa spreading factor: 7
-bw = 125000                                                     # Bandwidth: 125 kHz
-cr = 5                                                          # Coding rate: 4/5
-LoRa.setLoRaModulation(sf, bw, cr)
+    # Set TX power, default power for SX1262 and SX1268 are +22 dBm and for SX1261 is +14 dBm
+    # This function will set PA config with optimal setting for requested TX power
+    print("Set TX power to +22 dBm")
+    LoRa.setTxPower(22, LoRa.TX_POWER_SX1262)                       # TX power +17 dBm using PA boost pin
 
-# Configure packet parameter including header type, preamble length, payload length, and CRC type
-# The explicit packet includes header contain CR, number of byte, and CRC type
-# Receiver can receive packet with different CR and packet parameters in explicit header mode
-print("Set packet parameters:\n\tExplicit header type\n\tPreamble length = 12\n\tPayload Length = 15\n\tCRC on")
-headerType = LoRa.HEADER_EXPLICIT                               # Explicit header mode
-preambleLength = 12                                             # Set preamble length to 12
-payloadLength = 15                                              # Initialize payloadLength to 15
-crcType = True                                                  # Set CRC enable
-LoRa.setLoRaPacket(headerType, preambleLength, payloadLength, crcType)
+    # Configure modulation parameter including spreading factor (SF), bandwidth (BW), and coding rate (CR)
+    # Receiver must have same SF and BW setting with transmitter to be able to receive LoRa packet
+    print("Set modulation parameters:\n\tSpreading factor = 7\n\tBandwidth = 125 kHz\n\tCoding rate = 4/5")
+    sf = 7                                                          # LoRa spreading factor: 7
+    bw = 125000                                                     # Bandwidth: 125 kHz
+    cr = 5                                                          # Coding rate: 4/5
+    LoRa.setLoRaModulation(sf, bw, cr)
 
-# Set syncronize word for public network (0x3444)
-print("Set syncronize word to 0x3444")
-LoRa.setSyncWord(0x3444)
+    # Configure packet parameter including header type, preamble length, payload length, and CRC type
+    # The explicit packet includes header contain CR, number of byte, and CRC type
+    # Receiver can receive packet with different CR and packet parameters in explicit header mode
+    print("Set packet parameters:\n\tExplicit header type\n\tPreamble length = 12\n\tPayload Length = 15\n\tCRC on")
+    headerType = LoRa.HEADER_EXPLICIT                               # Explicit header mode
+    preambleLength = 12                                             # Set preamble length to 12
+    payloadLength = 15                                              # Initialize payloadLength to 15
+    crcType = True                                                  # Set CRC enable
+    LoRa.setLoRaPacket(headerType, preambleLength, payloadLength, crcType)
 
-print("\n-- LoRa Transmitter --\n")
+    # Set syncronize word for public network (0x3444)
+    print("Set syncronize word to 0x3444")
+    LoRa.setSyncWord(0x3444)
 
-# Message to transmit
-message = "HeLoRa World!\0"
-messageList = list(message)
-for i in range(len(messageList)) : messageList[i] = ord(messageList[i])
-counter = 0
+    print("\n-- LoRa Transmitter --\n")
 
-# Transmit message continuously
-while True :
+    # Message to transmit
+    message = "HeLoRa World!\0"
+    messageList = list(message)
+    for i in range(len(messageList)) : messageList[i] = ord(messageList[i])
+    counter = 0
 
-    # Transmit message and counter
-    # write() method must be placed between beginPacket() and endPacket()
-    LoRa.beginPacket()
-    LoRa.write(messageList, len(messageList))
-    LoRa.write([counter], 1)
-    LoRa.endPacket()
+    # Transmit message continuously
+    while True :
 
-    # Print message and counter
-    print(f"{message}  {counter}")
+        # Transmit message and counter
+        # write() method must be placed between beginPacket() and endPacket()
+        LoRa.beginPacket()
+        LoRa.write(messageList, len(messageList))
+        LoRa.write([counter], 1)
+        LoRa.endPacket()
 
-    # Wait until modulation process for transmitting packet finish
-    LoRa.wait()
+        # Print message and counter
+        print(f"{message}  {counter}")
 
-    # Print transmit time and data rate
-    print("Transmit time: {0:0.2f} ms | Data rate: {1:0.2f} byte/s".format(LoRa.transmitTime(), LoRa.dataRate()))
+        # Wait until modulation process for transmitting packet finish
+        LoRa.wait()
 
-    # Don't load RF module with continous transmit
-    time.sleep(5)
-    counter = (counter + 1) % 256
+        # Print transmit time and data rate
+        print("Transmit time: {0:0.2f} ms | Data rate: {1:0.2f} byte/s".format(LoRa.transmitTime(), LoRa.dataRate()))
 
-try :
-    pass
-except :
-    LoRa.end()
+        # Don't load RF module with continous transmit
+        time.sleep(5)
+        counter = (counter + 1) % 256
+
+if __name__ == "__main__":
+    try:
+        LoRa = SX126x()
+        main(LoRa)
+    except KeyboardInterrupt:
+        print("Program terminated by user")
+    finally:
+        LoRa.end()
