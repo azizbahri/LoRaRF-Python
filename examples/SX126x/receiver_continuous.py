e48c1c13e97f51ec8df7476b440ac4b4343c517f
aziz bahri <azizbahri90@gmail.com>
2024-02-15 23:41:39 +0000

    make receiver use yaml files

--- examples/SX126x/receiver_continuous.py
+++ examples/SX126x/receiver_continuous.py
@@ -1,5 +1,6 @@
 from LoRaRF import SX126x
 import argparse
+import yaml
 
 # Bit Masks
 # Bit mask for packet status
@@ -47,13 +48,20 @@ def receive_callback(self):
         message += chr(self.read())
     print(f"Received callback message")
     print(f"Message: {message}")
-    
+
     map_bits(self._statusIrq )
 
-def setup_lora(LoRa, f, sf, bw, cr, power, prot, preamble):
+# def setup_lora(LoRa, f, sf, bw, cr, power, prot, preamble,fdev):
+def setup_lora(LoRa, cfg):
     # Begin LoRa radio and set NSS, reset, busy, IRQ, txen, and rxen pin with connected Raspberry Pi gpio pins
     busId = 0; csId = 0
     resetPin = 18; busyPin = 20; irqPin = 16; txenPin = 6; rxenPin = -1
+    modem_cfg = cfg['modem_cfg']
+    packet_cfg = cfg['packet_cfg']
+
+    prot = modem_cfg['protocol']
+    freq = modem_cfg['frequency']
+
     print("Begin LoRa radio")
     if not LoRa.begin(busId, csId, resetPin, busyPin, irqPin, txenPin, rxenPin, prot=prot) :
         raise Exception("Something wrong, can't begin LoRa radio")
@@ -61,28 +69,38 @@ def setup_lora(LoRa, f, sf, bw, cr, power, prot, preamble):
     LoRa.onReceive(receive_callback)
     LoRa.setDio2RfSwitch()
     # Set frequency
-    print(f"Set frequency to {f} Hz")
-    LoRa.setFrequency(f)
+    print(f"Set frequency to {freq} Hz")
+    LoRa.setFrequency(freq)
 
     # Set RX gain to power saving gain
     print("Set RX gain to power saving gain")
     LoRa.setRxGain(LoRa.RX_GAIN_POWER_SAVING)
 
-    # Configure modulation parameter including spreading factor (SF), bandwidth (BW), and coding rate (CR)
-    print(f"Set modulation parameters:\n\tSpreading factor = {sf}\n\tBandwidth = {bw} Hz\n\tCoding rate = {cr}")
-    LoRa.setLoRaModulation(sf, bw, cr)
+    if prot == LoRa.FSK_MODEM:
+        print("Set FSK modulation parameters:\n\tFrequency deviation = {fdev} kHz\n\tBitrate = 50 kbps")
+        # bitrate = 50000
+        # LoRa.setFskModulation(bitrate, LoRa.PULSE_NO_FILTER,LoRa.BW_46900,fdev )
 
-    # Configure packet parameter including header type, preamble length, payload length, and CRC type
-    print("Set packet parameters:\n\tExplicit header type\n\tPreamble length = 12\n\tPayload Length = 15\n\tCRC on")
-    headerType = LoRa.HEADER_EXPLICIT
-    preambleLength = preamble
-    payloadLength = 15
-    crcType = True
-    LoRa.setLoRaPacket(headerType, preambleLength, payloadLength, crcType)
+        # LoRa.setFskPacket()
+    elif prot == LoRa.LORA_MODEM:
+        sf = modem_cfg['spreading_factor']
+        bw = modem_cfg['bandwidth']
+        cr = modem_cfg['coding_rate']
+        print(f"Set modulation parameters:\n\tSpreading factor = {sf}\n\tBandwidth = {bw} Hz\n\tCoding rate = {cr}")
 
-    # Set syncronize word for public network (0x3444)
-    print("Set syncronize word to 0x3444")
-    # LoRa.setSyncWord(0x3444)
+        LoRa.setLoRaModulation(sf, bw, cr)
+
+        # Configure packet parameter including header type, preamble length, payload length, and CRC type
+        print("Set packet parameters:\n\tExplicit header type\n\tPreamble length = 12\n\tPayload Length = 15\n\tCRC on")
+        headerType = LoRa.HEADER_EXPLICIT
+        preambleLength = packet_cfg['preamble_length']
+        payloadLength = 15
+        crcType = True
+        LoRa.setLoRaPacket(headerType, preambleLength, payloadLength, crcType)
+
+        # Set syncronize word for public network (0x3444)
+        print("Set syncronize word to 0x3444")
+        LoRa.setSyncWord(0x3444)
 
     print("\n-- LoRa Receiver Continuous --\n")
 
@@ -123,9 +141,16 @@ def setup_lora(LoRa, f, sf, bw, cr, power, prot, preamble):
     #         if status == LoRa.STATUS_HEADER_ERR : print("Packet header error")
 
 
+def read_configurations(file_path):
+    with open(file_path, 'r') as file:
+        configurations = yaml.safe_load(file)
+    return configurations
+
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description="LoRa Receiver Continuous")
+    parser.add_argument('--cfg', help='Path to the configuration file')
     parser.add_argument("--f", type=int, default=868000000, help="Frequency in Hz")
     parser.add_argument("--sf", type=int, default=7, help="Spreading factor")
     parser.add_argument("--bw", type=int, default=125000, help="Bandwidth in Hz")
@@ -133,10 +158,13 @@ if __name__ == "__main__":
     parser.add_argument("--power", type=int, default=22, help="Transmit power in dBm")
     parser.add_argument("--prot", type=int, default=0, help="Protocol 0: LoRa, 1: FSK")
     parser.add_argument("--preamble", type=int, default=12, help="Preamble length")
+    parser.add_argument("--fdev", type=int, default=25000, help="Frequency deviation in Hz")
     args = parser.parse_args()
 
+    if args.cfg:
+        configurations = read_configurations(args.cfg)
     LoRa = SX126x()
     try:
-        setup_lora(LoRa,args.f, args.sf, args.bw, args.cr, args.power, args.prot, args.preamble)
+        setup_lora(LoRa, configurations)
     except:
         LoRa.end()
